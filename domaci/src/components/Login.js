a7655145b4e0409fb8d5f9463678def060639e49
andrea9888 <andrea98.com@gmail.com>
2020-06-20 20:11:08 +0200

    Routes added, ant design incorporated for sider on dashboard page

---
+++ domaci/src/components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class Login extends React.Component {
+    state = {  }
+    render() { 
+        return ( <div>Login</div> );
+    }
+}
+ 
+export default Login;
---
+++ domaci/src/components/components-dashboard/Hosting.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Hosting extends Component {
+    state = {  }
+    render() { 
+        return (<div>Hosting</div> );
+    }
+}
+ 
+export default Hosting;
